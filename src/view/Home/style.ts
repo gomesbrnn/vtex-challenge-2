f38e773bbb9a554d16f7d74d217d7ca67500af89
gomesbrnn <brngomesds@gmail.com>
2021-07-23 22:05:11 -0300

    fix products style

--- src/view/Home/style.ts
+++ src/view/Home/style.ts
@@ -12,21 +12,24 @@ margin: auto;
     .cart {
        display: flex;
        justify-content: center;
-       align-items: center; 
+       align-items: center;
+       padding-right: 3.5em;
     }
 }
 
 
 section {
-    height: 100vh;
-    width: 100%;
+    height: 100%;
+    width: 95%;
     background: #e2e2;
     display: flex;
-    justify-content: space-around;
+    
+    flex-wrap: wrap;
 
     .product-content{
         display: grid;
         text-align: center;
+        margin: 1em;
         height: 300px;
         background: #fff;
         border-radius: 12px;
